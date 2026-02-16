bc83754bda9def6f9661eaa09207c85bf08274ee
justagame-dev <fkmbloodshead@gmail.com>
2026-02-16 21:46:30 +1100

    Moved things around

--- Js/index.js
+++ Js/index.js
@@ -186,7 +186,7 @@ function launchGame(gameType) {
         titleEl.innerHTML = '<i class="fa-solid fa-cube text-indigo-400 mr-2"></i> CUBE COMBAT';
         hintEl.innerHTML = 'Physics Arena';
         iframe.classList.remove('hidden');
-        iframe.src = 'Games/Cube-combat.html';
+        iframe.src = 'Games/Full/cube-combat.html';
         
         startBtn.classList.add('hidden');
         statusTitle.textContent = "";
@@ -196,7 +196,7 @@ function launchGame(gameType) {
         titleEl.innerHTML = '<i class="fa-solid fa-dice text-red-400 mr-2"></i> SKETCHY CASINO';
         hintEl.innerHTML = 'Try Your Luck';
         iframe.classList.remove('hidden');
-        iframe.src = 'Games/Sketchy-Caisno.html';
+        iframe.src = 'Games/Full/Sketchy-Casino.html';
         
         startBtn.classList.add('hidden');
         statusTitle.textContent = "";
